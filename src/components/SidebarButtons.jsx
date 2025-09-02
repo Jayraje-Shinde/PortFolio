ae5ea7c8c15578dbabb9db5082693a8b600ce6c6
Jayraje Shinde <zeyraje@gmail.com>
2025-09-02 14:27:02 +0530

    changed font size in sidebarbuttons.jsx

--- src/components/SidebarButtons.jsx
+++ src/components/SidebarButtons.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom"
 export function SidebarItemsWithStyles() {
 	return (
 		<div className="my-4 h-full flex flex-col text-creamewhite items-center py-6 ">
-			<Link to="/"><div className="my-3 p-4 hover:bg-teracottabg-2  rounded-2xl">Home</div></Link>
-			<Link to="/About"><div className="my-3 p-4 hover:bg-teracottabg-2 rounded-2xl">About</div></Link>
-			<Link to="/Projects"><div className="my-3 p-4 hover:bg-teracottabg-2 rounded-2xl">Projects</div></Link>
-			<Link to="/Contact"><div className="my-3 p-4 hover:bg-teracottabg-2 rounded-2xl">Contact</div></Link>
+			<Link to="/"><div className="my-3 p-3 text-[20px] hover:bg-teracottabg-2  rounded-2xl">Home</div></Link>
+			<Link to="/About"><div className="my-3 p-3 text-[20px] hover:bg-teracottabg-2 rounded-2xl">About</div></Link>
+			<Link to="/Projects"><div className="my-3 p-3 text-[20px] hover:bg-teracottabg-2 rounded-2xl">Projects</div></Link>
+			<Link to="/Contact"><div className="my-3 p-3 text-[20px] hover:bg-teracottabg-2 rounded-2xl">Contact</div></Link>
 		</div >
 	)
 } 
